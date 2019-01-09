09ca8ed3e063064c89af529b39875512967f283d
Pablo Henao <pabloh1992@gmail.com>
2019-01-09 12:22:52 +0100

    version b5 to be merged

--- pyramid.c
+++ pyramid.c
@@ -5,7 +5,7 @@ int main()
 
     printf("Enter number of rows: ");
     scanf("%d",&rows);
-
+    printf("Version B5 to be merged: ");
     for(i=1; i<=rows; ++i)
     {
         for(j=1; j<=i; ++j)
