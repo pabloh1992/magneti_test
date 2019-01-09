1b6701faf2dfcfdfde34fbb2d8d41dd80ae120ee
Pablo Henao <pabloh1992@gmail.com>
2019-01-09 14:37:20 +0100

    B6 version confirmed

--- pyramid.c
+++ pyramid.c
@@ -2,17 +2,13 @@
 int main()
 {
     int i, j, rows;
-
+    printf("Version B6 TO BE MERGED: ");	
     printf("Enter number of rows: ");
     scanf("%d",&rows);
 
     for(i=1; i<=rows; ++i)
     {
-        for(j=1; j<=i; ++j)
-        {
             printf("* ");
-        }
-        printf("\n");
     }
     return 0;
 }
